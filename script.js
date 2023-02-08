64219a10f7069eddb5f8f3283472f1b4704bfc0b
KGrirogyy <kachurovskyy123@gmail.com>
2023-02-08 17:30:12 +0200

    trims added

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 let numberOfFilms;
 
 function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?" + "");
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?" + "").trim();
 
   while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?" + "");
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?" + "").trim();
   }
 }
 
@@ -22,8 +22,8 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
   for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?" + ""),
-      b = +prompt("На сколько оцените его?" + "");
+    const a = prompt("Один из последних просмотренных фильмов?" + "").trim(),
+      b = +prompt("На сколько оцените его?" + "").trim();
 
     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
       personalMovieDB.movies[a] = b;
@@ -49,7 +49,9 @@ function detectPersonalLevel() {
 
 function writeYourGenres() {
   for (let i = 1; i < 4; i++) {
-    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = prompt(
+      `Ваш любимый жанр под номером ${i}`
+    ).trim();
   }
 }
 writeYourGenres();
